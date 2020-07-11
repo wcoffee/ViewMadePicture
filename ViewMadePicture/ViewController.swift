4e1bdf83c0c4f2ee727d91fc7265df86312569c9
FC <wcoffee995@gmail.com>
2020-07-11 13:33:37 +0800

    Initial Commit

---
+++ ViewMadePicture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewMadePicture
+//
+//  Created by Freda Cheng on 2020/7/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
